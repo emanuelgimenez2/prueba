fd20328ec656634a7153bf22730a500314c26b11
emanuelgimenez2 <emanuelgimenez2@gmail.com>
2024-06-09 21:43:04 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Mi Primer Proyecto de React</h1>
+      <img src='https://th.bing.com/th/id/OIG4.s0LaWgxduT.2pMIK9nPU?pid=ImgGn' alt='Descripción de la imagen' />
+    </div>
+  );
+}
+
+export default App;
